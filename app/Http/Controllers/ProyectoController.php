9c8dad5fd778f6bfd5582d32f5af380d4dd55a74
Eduardo Yaguar <eduardoyaguarf@gmail.com>
2024-10-08 07:49:28 -0500

    Initial commit

---
+++ app/Http/Controllers/ProyectoController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Proyecto;
+use Illuminate\Support\Facades\DB;
+
+class ProyectoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $proyectos = DB::table('proyectos') -> get();
+        return view("projects/index", ['proyectos' => $proyectos]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view("projects/new");
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        Proyecto::create($request -> all());
+        return redirect('project/') 
+            -> with('success', 'Proyecto creado satisfactoriamente.');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $proyecto = Proyecto::find($id);
+        return view('projects/update', compact('proyecto'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $request -> validate([
+            'titulo'=> 'required|max:255',
+            'descripcion'=> 'required'
+        ]);
+        $proyecto = Proyecto::find($id);
+        $proyecto -> update($request->all());
+        return redirect('project/') 
+            -> with('success', 'Proyecto actualizado satisfactoriamente.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $proyecto = Proyecto::find($id);
+        $proyecto->delete();
+        return redirect('project/') 
+        -> with('success', 'Proyecto eliminado satisfactoriamente;');
+    }
+}
---
+++ resources/views/projects/update.blade.php
@@ -0,0 +1,99 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Insert demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+
+    <div class="container text-center">
+        <div class="row">
+            <div class="col">
+                <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                    <div class="container-fluid">
+                        <a class="navbar-brand" href="#">Navbar</a>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                                <li class="nav-item">
+                                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Link</a>
+                                </li>
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                        Dropdown
+                                    </a>
+                                    <ul class="dropdown-menu">
+                                        <li><a class="dropdown-item" href="#">Action</a></li>
+                                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                                        <li>
+                                            <hr class="dropdown-divider">
+                                        </li>
+                                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                    </ul>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                                </li>
+                            </ul>
+                            <form class="d-flex" role="search">
+                                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                                <button class="btn btn-outline-success" type="submit">Search</button>
+                            </form>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col">
+                <ul class="nav flex-column">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="{{url('project/') }}">Proyectos</a>
+                    </li>
+                </ul>
+            </div>
+
+            
+            <div class="col-9">
+            <h1 class="mt-3">Modificación de Proyectos</h1>
+                <form action="{{ route( 'project.update', $proyecto -> id ) }}" method="post">
+                    @csrf
+                    @method('PUT')
+                    <div class="input-group mb-3 mt-5">
+                        <span class="input-group-text" id="inputGroup-sizing-default">ID</span>
+                        <input disabled id="id" name="id" type="text" class="form-control" value="{{$proyecto->id}}" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+                    </div>
+                    <div class="input-group mb-3 mt-5">
+                        <span class="input-group-text" id="inputGroup-sizing-default">Título</span>
+                        <input id="titulo" name="titulo" type="text" value="{{$proyecto->titulo}}" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+                    </div>
+                    <div class="form-floating">
+                        <textarea class="form-control" placeholder="Leave a comment here" name="descripcion" id="descripcion">{{$proyecto->descripcion}}</textarea>
+                        <label  for="floatingTextarea">Descripción</label>
+                    </div>
+                    <button class="btn btn-primary mt-5" type="submit">Guardar</button>
+                </form>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
