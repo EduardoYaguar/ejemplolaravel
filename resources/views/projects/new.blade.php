6e80dab9e46aceca8c02825dcffd89c1b4ec16d5
Eduardo Yaguar <eduardoyaguarf@gmail.com>
2024-10-08 08:01:37 -0500

    Update index
    Add nav-item to register a new project

--- resources/views/projects/new.blade.php
+++ resources/views/projects/new.blade.php
@@ -60,6 +60,9 @@
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="{{url('project/') }}">Proyectos</a>
                     </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="{{url('project/create') }}">Registrar Proyecto</a>
+                    </li>
                 </ul>
             </div>
 
